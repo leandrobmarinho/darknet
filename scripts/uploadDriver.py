e38f299942916c075beeddbbfa09780d4e010687
Leandro Bezerra Marinho <leandro.marinho@lapisco.ifce.edu.br>
2020-03-25 18:29:39 -0300

    withou upload drive

--- scripts/uploadDriver.py
+++ scripts/uploadDriver.py
@@ -3,7 +3,6 @@ from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
-import argparse
 
 import time
 import datetime
@@ -30,31 +29,15 @@ def get_filename():
   st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M')
   return st
 
-def upload_weight2drive(filename, out_drive_folder_id):
-  # Create & upload a text file.
-  # uploaded = drive.CreateFile({})
-  uploaded = drive.CreateFile({"parents": [{"kind": "drive#fileLink",
-                                        "id": out_drive_folder_id}],
-                               'title': filename.split("/")[-1]})
-  uploaded.SetContentFile(filename)
-  uploaded.Upload()
-  print('Uploaded file with ID {}'.format(uploaded.get('id')))
 
-if __name__ == '__main__':
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-f", "--filename", required=True, help="Filename for upload", type=str)
-    args = vars(ap.parse_args())
+# Authenticate and create the PyDrive client.
+# This only needs to be done once per notebook.
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
 
-    filename = args['filename']
-    out_drive_folder_id = os.environ.get('OUTPUT_FOLDER_ID')
-    # print("Exporting to Drive: {} {}".format(filename, out_drive_folder_id)
-
-    # Authenticate and create the PyDrive client.
-    # This only needs to be done once per notebook.
-    auth.authenticate_user()
-    gauth = GoogleAuth()
-    gauth.credentials = GoogleCredentials.get_application_default()
-    drive = GoogleDrive(gauth)
-
-    upload_weight2drive(filename, out_drive_folder_id)
+weights_name = '/content/darknet/backup'
+#out_drive_folder_id = '1LvWCZ5hX7xjnRV0_p9w22f4zsg12Qzxx'
+out_drive_folder_id = os.environ.get('OUTPUT_FOLDER_ID')
+export_folder(weights_name, out_drive_folder_id)
